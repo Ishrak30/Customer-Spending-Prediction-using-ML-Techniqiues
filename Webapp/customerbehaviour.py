736574ccc44460c0ffd41f1e45e52808bb9a8cc7
Ishrak_Hussain <ishrakhussain30@gmail.com>
2023-03-26 01:47:25 +0600

    webapp done functionally

---
+++ Webapp/customerbehaviour.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pickle
+from flask import Flask, request, render_template
+
+model = pickle.load(open(
+    'D:/Github/Customer Behaviour Analysis/webapp-pickle-dtm', 'rb'))
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    # Put all form entries values in a list
+    features = [float(i) for i in request.form.values()]
+    # Convert features to array
+    array_features = [np.array(features)]
+    print(array_features)
+    # Predict features
+    prediction = model.predict(array_features)
+    print(prediction)
+
+    return render_template('home.html', result=prediction)
+
+
+if __name__ == '__main__':
+    app.run()
